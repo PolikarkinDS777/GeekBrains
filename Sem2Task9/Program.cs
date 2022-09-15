aa1f169e85a57be823422e6f7f4271568cdb7493
Eduard <mozg780@yandex.ru>
2022-09-15 05:59:25 +0300

    add completed tasks

--- Sem2Task9/Program.cs
+++ Sem2Task9/Program.cs
@@ -4,7 +4,6 @@
 //======================================================================
 
 
-void TernaryVariant1()
 {
     Console.WriteLine("\nВариант 2 (тернарный оператор)");
     System.Random numberGenerator = new System.Random();
@@ -19,34 +18,3 @@ void TernaryVariant1()
     Console.Write("\nНаибольшая цифра = ");
     Console.WriteLine((firstDigit > secondDigit) ? firstDigit : secondDigit);
 }
-
-void TernaryVariant3()
-{
-    Console.WriteLine("\nВариант 3 (тернарный оператор с выводом первая или вторая цифра)");
-    System.Random numberGenerator = new System.Random();
-    int number = numberGenerator.Next(10, 100);
-    Console.WriteLine("\nСгенерированное случайное число = " + number);
-
-    int firstDigit = number/10;
-    Console.WriteLine("\nfirstDigit = " + firstDigit);
-    int secondDigit = number%10;
-    Console.WriteLine("secondDigit = " + secondDigit);
-    
-    Console.WriteLine((firstDigit > secondDigit) ? "\nНаибольшая firstDigit = " + firstDigit : "\nНаибольшая secondDigit = " + secondDigit);
-}
-
-void CharVariant4()
-{
-    Console.WriteLine("\nВариант 4 (через char)");
-    System.Random numberGenerator = new System.Random();
-    int number = numberGenerator.Next(10, 100);
-    Console.WriteLine("\nСгенерированное случайное число = " + number);
-    
-    char[] charArray = number.ToString().ToCharArray();
-    Console.Write("\nНаибольшее = ");
-    Console.WriteLine(((int)charArray[0] > (int)charArray[1]) ? charArray[0] : charArray[1]);
-}
-
-TernaryVariant2();
-TernaryVariant3();
-CharVariant4();
